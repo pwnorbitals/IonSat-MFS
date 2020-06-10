e48b2373316d0f3755a30133c1ca41c150ece951
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2020-06-10 14:03:12 +0530

    Basic Meson setup

---
+++ FFS/src/FFS.h
@@ -0,0 +1,10 @@
+#ifndef FFS_HPP_INC
+#define FFS_HPP_INC
+
+#include <iostream>
+
+namespace FFS {
+    void test() { std::cout << "FFS test" << std::endl; }
+}
+
+#endif
---
+++ MFS/src/mfs.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "FFS.h"
+
+int main() {
+    std::cout << "test" << std::endl;
+    FFS::test();
+
+    return 0;
+}
