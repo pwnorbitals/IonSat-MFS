3f3ac6b3dd7d612c0f21e7a88018017cb2b9495b
Chris [pwnOrbitals] de CLAVERIE <c.de-claverie@pm.me>
2020-08-24 20:10:53 +0200

    more improvements

--- src/MFS/mfs.cpp
+++ src/MFS/mfs.cpp
@@ -7,32 +7,34 @@
 
 
 struct TimerEvent{ std::clock_t emissionTime; };
-struct RequirePropagate{ Tle TwoLineElements; DateTime EndTime; };
+struct RequirePropagate{ };
 
 void propagator(RequirePropagate const& propagation) {
     std::cout << "Initializing propagator" << std::endl;
-    SGP4 sgp4(propagation.TwoLineElements);
-    auto start = std::clock();
+    auto tle = Tle{"ISS (ZARYA)", "1 25544U 98067A   20237.19927083  .00000402  00000-0  15362-4 0  9990", "2 25544  51.6471  11.8824 0001577  59.0436 306.4808 15.49181383242568"};
+    auto end = tle.Epoch().AddHours(6);
+    SGP4 sgp4(tle);
+    auto start = std::chrono::high_resolution_clock::now();
 
     std::cout << "Starting propagation" << std::endl;
-    auto calculated_pos = sgp4.FindPosition(propagation.EndTime);
-    std::cout << calculated_pos.ToGeodetic();
+    auto calculated_pos = sgp4.FindPosition(end);
+    std::cout << calculated_pos.ToGeodetic() << std::endl;
 
-    auto duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-    std::cout << "Propagation time : " << duration << std::endl;
+    auto duration = std::chrono::duration<double, std::milli>{std::chrono::high_resolution_clock::now() - start};
+    std::cout << "Propagation time : " << duration.count()/1000 << std::endl;
 
 
 }
 
 void timerHandler(TimerEvent const& evt) {
-    auto tle = Tle{"ISS (ZARYA)", "1 25544U 98067A   20237.19927083  .00000402  00000-0  15362-4 0  9990", "2 25544  51.6471  11.8824 0001577  59.0436 306.4808 15.49181383242568"};
-    RFF::emit(RequirePropagate{tle, tle.Epoch().AddHours(6)});
+    
+    RFF::emit(RequirePropagate{});
 }
 
 void timer_15sec(void*) {
     auto start = std::chrono::high_resolution_clock::now();
     while(1) {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        RFF::Task<1, 2048>::delay(100);
         auto now = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration<double, std::milli>{now-start};
         std::cout << duration.count() << std::endl;
@@ -43,7 +45,7 @@ void timer_15sec(void*) {
     }
 }
 
-RFF::Task<1, 2048> timerTask{timer_15sec, "TimerTask"};
+RFF::Task<9, 2048> timerTask{timer_15sec, "TimerTask"};
 
 RFF::EventHandler<TimerEvent, 2> PropagatorTimer{timerHandler};
 RFF::EventHandler<RequirePropagate> Propagator{propagator};
