e9830d6c3f94250c3d50112b340f6c1f4a0fb92d
kmgx <kmgx@declaverie.tech>
2020-08-31 18:41:26 +0200

    blink test

---
+++ src/MFS/blink_test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "RFF.h"
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+
+void blinker(void*) {
+    while(1) {
+        gpio_toggle(GPIOG, GPIO13 | GPIO14);	
+        RFF::delay(1 * configTICK_RATE_HZ); 
+    }
+}
+
+RFF::Task<9, 10000> blinkerTask{blinker, "BlinkerTask"};
+
+
+RFF::Setup setup{};
+
+void rff_main() {
+    rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+    rcc_periph_clock_enable(RCC_GPIOG);
+    gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13 | GPIO14);
+}
