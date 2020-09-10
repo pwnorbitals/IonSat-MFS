4cdcf7872c129dd7eae8a119bcdea92a301ba6c0
kmgx <kmgx@declaverie.tech>
2020-09-10 22:10:14 +0200

    try uart stdio

--- src/tests/blink_test.cpp
+++ src/tests/blink_test.cpp
@@ -3,22 +3,45 @@
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/usart.h>
+
+int __io_putchar(int ch) {
+    usart_send(USART1, ch);
+    return 0;
+}
 
 void blinker(void*) {
     while(1) {
         gpio_toggle(GPIOG, GPIO13 | GPIO14);	
+        std::cout << "t";
         RFF::delay(1 * configTICK_RATE_HZ); 
     }
 }
-
+ // TODO : switch to timers
 RFF::Task<9, 10000> blinkerTask{blinker, "BlinkerTask"};
-
 
 RFF::Setup setup{};
 
 void rff_main() {
     rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+
+
     rcc_periph_clock_enable(RCC_GPIOG);
+    rcc_periph_clock_enable(RCC_USART1);
+
     gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13 | GPIO14);
     gpio_toggle(GPIOG, GPIO13 | GPIO14);
+
+    
+    
+    /* Setup USART1 parameters. */
+	usart_set_baudrate(USART1, 115200);
+	usart_set_databits(USART1, 8);
+	usart_set_stopbits(USART1, USART_STOPBITS_1);
+	usart_set_mode(USART1, USART_MODE_TX);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+
+	/* Finally enable the USART. */
+	usart_enable(USART1);
 }
--- test/phys/blink.cpp
+++ test/phys/blink.cpp
@@ -10,13 +10,13 @@
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-
+#include <libopencm3/stm32/usart.h>
 
 struct BlinkEvent { int eventNo; };
 
 void blinker(BlinkEvent const& evt) {
     gpio_toggle(GPIOG, GPIO13);
-    
+    std::cout << "t";
 }
 
 
@@ -40,8 +40,33 @@ FFS::Setup setup{std::make_tuple(FFS::Mode{"abc"}), m1};
 
 FFS::Task<1, 512> t1{timer, "timer"};
 
+int __io_putchar(int ch) {
+    uart_send(UART1, ch);
+    return 0;
+}
+
 void ffs_main() {
     rcc_periph_clock_enable(RCC_GPIOG); // Enable clock GPIOC
+
     gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13); // Configure GPIO13
+
+    // Enable the UART clock
+    periph_clock_enable(RCC_UART1);
+    // We need a brief delay before we can access UART config registers
+    __asm__("nop"); __asm__("nop"); __asm__("nop");
+    // Disable the UART while we mess with its settings
+    uart_disable(UART1);
+    // Configure the UART clock source as precision internal oscillator
+    uart_clock_from_piosc();
+    // Set communication parameters
+    uart_set_baudrate(UART1, 115200);
+    uart_set_databits(UART1, 8);
+    uart_set_parity(UART1, UART_PARITY_NONE);
+    uart_set_stopbits(UART1, 1);
+    // Enable RTC and CTS lines
+    uart_set_flow_control(UART1, UART_FLOWCTL_HARD_RTS_CTS);
+    // Now that we're done messing with the settings, enable the UART
+    uart_enable(UART1);
+
 }
 
